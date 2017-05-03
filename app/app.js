1e5c116f4fb17ae941aac8d390b2f9e3fcd54b59
Michael Watson <m.watson.tn@gmail.com>
2017-05-03 14:20:45 -0500

    set up homectrl and view

--- app/app.js
+++ app/app.js
@@ -1,7 +1,13 @@
 // console.log('app.js')
 const app = angular.module('Snap', ['ngRoute'])
 
-app.config(['$routeProvider', function($routeProvider) {
+app.config(function($routeProvider, $locationProvider) {
+	$locationProvider.hashPrefix('')
 	$routeProvider
-	}
-])
+	.when('/', {
+		templateUrl: 'partials/home.html',
+		controller: 'HomeCtrl'
+	}).otherwise({
+		redirectTo: '/'
+	})
+})
