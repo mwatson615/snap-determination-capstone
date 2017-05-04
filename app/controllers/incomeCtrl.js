44a29460d3bd8fc0e84a68ff4e2545213576f3ae
Michael Watson <m.watson.tn@gmail.com>
2017-05-04 13:34:01 -0500

    linked materialize js and added empl, pay period fields on ic view

--- app/controllers/incomeCtrl.js
+++ app/controllers/incomeCtrl.js
@@ -1,3 +1,7 @@
 app.controller('IncomeCtrl', function($scope) {
+	$(document).ready(function() {
+		$('select').material_select();
+	});
 	console.log('income ctrl')
+	// $scope.selectOptions = ["weekly", "biweekly", "twiceMonthly", "monthly"]
 })
