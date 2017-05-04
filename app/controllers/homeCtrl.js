be607da2578eb3f95f7217f3c832d1af1decdbfe
Michael Watson <m.watson.tn@gmail.com>
2017-05-03 21:43:36 -0500

    basic outline for demo, routes work via nav but not directly

--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -1,3 +1,5 @@
-app.controller('HomeCtrl', function($scope) {
-	console.log('home ctrl')
+app.controller('HomeCtrl', function($scope, $location) {
+	$scope.proceed = () => {
+		$location.url('/demo')
+	}
 })
