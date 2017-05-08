991238c8ce4b21f617f68f553e09b60ca3943703
Michael Watson <m.watson.tn@gmail.com>
2017-05-08 15:14:44 -0500

    created snapdb and connected via robomongo, home route and initial router

--- server/server.js
+++ server/server.js
@@ -3,16 +3,30 @@
 const express = require('express');
 const { json } = require('body-parser');
 const mongoose = require('mongoose');
+const { Router } = require('express');
+const router = Router();
+
+const routes = require('../server/routes/router.js')
 
 const app = express();
 const PORT = process.env.PORT || 3000
 
-app.use(json());
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/snapdb'
+
+// app.use(json());
 
 mongoose.Promise = Promise;
 
-app.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}`)
-})
+
+
+router.use('/api/v1', routes)
+
+mongoose.connect(MONGODB_URL)
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`Listening on port ${PORT}`)
+		})
+	})
+	.catch(console.error)
 
 module.exports = app;
