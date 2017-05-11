9deceb840d3f4e5803505d669ea3be2095b3d455
Michael Watson <m.watson.tn@gmail.com>
2017-05-11 10:39:05 -0500

    added calc formula for monthly IC on IC view

--- client/app/controllers/incomeCtrl.js
+++ client/app/controllers/incomeCtrl.js
@@ -2,31 +2,40 @@ app.controller('IncomeCtrl', function($scope) {
 	$(document).ready(function() {
 		$('select').material_select();
 	});
-	let paystubArr = [];
 	$scope.payArray = [];
-	// $scope.show2 = false;
-	// $scope.show4 = false;
-	// $scope.show8 = false;
-	// $scope.payFrequency = '';
+	let divider = 8,
+	multiplier = 4.3,
+	monthlyIncome = '';
 
-	$scope.getPaystubs = (paystubs) => {
-		for ( let i = 0; i < paystubs.length; i++) {
-			let payTest = parseInt(paystubs[i].value)
-			if (payTest[i] !== 'NaN' || null) {
-			// 	console.log(typeof paystubs[i].value)
-			paystubArr.push(parseInt(payTest));
-		}
+	$scope.getSum = () => {
+		$scope.totalIncome = $scope.payArray.reduce(function(acc, val) {
+		return acc + val;
+		}, 0);
 	}
-	console.log(paystubArr)
+
+	$scope.calculateMonthly = () => {
+		if ($scope.payFrequency === 'biweekly') {
+			divider = 4;
+			multiplier = 2.15;
+		} else if ($scope.payFrequency === 'twiceMonthly') {
+			divider = 4;
+			multiplier = 2;
+		} else if ($scope.payFrequency === 'monthly') {
+			divider = 2;
+			multiplier = 1;
+		}
+	monthlyIncome = ($scope.totalIncome / divider) * multiplier
 	}
 
 	$scope.getIncome = () => {
-		// $scope.getPaystubs($('.paystubs'))
-		// let employment = {
-		// 	"employer": $scope.employer,
-		// 	"payFrequency": $scope.payFrequency,
-		// 	"payStubs": paystubArr
-		// }
-		console.log($scope.payArray)
+		$scope.getSum()
+		$scope.calculateMonthly()
+		let employment = {
+			"employer": $scope.employer,
+			"payFrequency": $scope.payFrequency,
+			"payStubs": $scope.payArray,
+			"monthlyIncome": monthlyIncome
+		}
+		console.log(employment)
 	}
 })
