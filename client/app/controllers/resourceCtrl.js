b9a817ca33f93304564c460993f2e8d689d8b468
Michael Watson <m.watson.tn@gmail.com>
2017-05-14 18:12:21 -0500

    fixed ngrepeat on resource view, values now showing in console log

--- client/app/controllers/resourceCtrl.js
+++ client/app/controllers/resourceCtrl.js
@@ -1,41 +1,43 @@
 app.controller('ResourceCtrl', function($scope, $cookies, personFactory) {
-	// 	$(document).ready(function() {
+	// $(document).ready(function() {
 	// 	$('select').material_select();
 	// });
 
 	let householdId = $cookies.get('householdId')
 	console.log(householdId)
-	$scope.people = [];
-	$scope.personId = [];
+	$scope.personArray = [];
+	let personIdArray = [];
 
 	personFactory.getPersonByHousehold(householdId)
 	.then((data) => {
+// //TODO : ONLY ONE LINE PER ADULT, REWORK PARTIAL TO JUST ASK TOTAL PER ADULT
+		console.log(data)
 		for (i = 0; i < data.length; i++) {
-			$scope.people.push(data[i]);
-			$scope.personId[i] = data[i]._id;
-		console.log($scope.people)
+			$scope.personArray.push(data[i]);
+			personIdArray[i] = data[i]._id;
+		console.log($scope.personArray)
 		}
 	})
-	.then(() => {
-		$(document).ready(function() {
-			$('select').material_select();
-		});
-	})
+	// .then(() => {
+	// 	$(document).ready(function() {
+	// 		$('select').material_select();
+	// 	});
+	// })
 
 	// let resourceType = [],
-	// 	resourceBalance = [];
+	// let resourceBalance = [];
 
-	// $scope.resType = [];
-	// $scope.resBalance = [];
-	$scope.saveResources = () => {
-	// 	for (i = 0; i < $scope.resType.length; i++) {
-	// 	resourceType.push($scope.resType[i])
-	// 	resourceBalance.push($scope.resBalance[i])
+	// $scope.resType = '';
+	// $scope.resBalance = '';
+	$scope.saveResources = (balance) => {
+	// 	for (i = 0; i < $scope.people.length; i++) {
+	// // 	resourceType.push($scope.resType[i])
+	// 	resourceBalance.push($scope.resBalance)
 	// }
 		let resources = {
-			"personId": $scope.personId,
-			"resourceType" : $scope.resType,
-			"resourceBalance" : $scope.resBalance
+			"personId": personIdArray,
+			// "resourceType" : $scope.resTyp,
+			"resourceBalance": $scope.balance
 		}
 		console.log(resources)
 		// personFactory.addResource(resources)
