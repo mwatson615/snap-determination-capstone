d8aec84650d4d9fc1a5aaf0c5b3e8a5695cd9f04
Michael Watson <m.watson.tn@gmail.com>
2017-05-17 09:44:21 -0500

    can add multiple ppl on demo view, posts to db

--- client/app/controllers/demoCtrl.js
+++ client/app/controllers/demoCtrl.js
@@ -1,17 +1,81 @@
 app.controller('DemoCtrl', function($scope, $route, personFactory, $location, $cookies) {
 
 	$scope.householdId = $cookies.get('householdId')
-	$scope.hasSSN = false;
-	$scope.hasResource = false;
-	$scope.hasEmployer = false;
-	$scope.personArray = []
-	console.log($scope.householdId)
+	// $scope.firstName = [];
+	// $scope.hasResource = [];
+	// $scope.hasEmployer = [];
+	// $scope.age = [];
+
+$scope.people = [{}]
+
+	// for (let i = 0; i < 8; i++) {
+	// $scope.hasResource = false;
+	$scope.people.hasEmployer = false;
+	// $scope.firstName = '';
+	// $scope.age[i = '';
+	let personArray = []
+	// console.log($scope.)
+	// }
+	let myArray = [];
+
+	$scope.getPeople = () => {
+		$scope.people = [
+			{
+				firstName: $scope.firstName1,
+				age: $scope.age1,
+				hasResource: $scope.hasResource1 || false,
+				hasEmployer: $scope.hasEmployer1 || false
+			},
+			{
+				firstName: $scope.firstName2,
+				age: $scope.age2,
+				hasResource: $scope.hasResource2 || false,
+				hasEmployer: $scope.hasEmployer2 || false
+			},
+			{
+				firstName: $scope.firstName3,
+				age: $scope.age3,
+				hasResource: $scope.hasResource3 || false,
+				hasEmployer: $scope.hasEmployer3 || false
+			},
+			{
+				firstName: $scope.firstName4,
+				age: $scope.age4,
+				hasResource: $scope.hasResource4 || false,
+				hasEmployer: $scope.hasEmployer4 || false
+			},
+			{
+				firstName: $scope.firstName5,
+				age: $scope.age5,
+				hasResource: $scope.hasResource5 || false,
+				hasEmployer: $scope.hasEmployer5 || false
+			},
+			{
+				firstName: $scope.firstName6,
+				age: $scope.age6,
+				hasResource: $scope.hasResource6 || false,
+				hasEmployer: $scope.hasEmployer6 || false
+			},
+			{
+				firstName: $scope.firstName7,
+				age: $scope.age7,
+				hasResource: $scope.hasResource7 || false,
+				hasEmployer: $scope.hasEmployer7 || false
+			},
+			{
+				firstName: $scope.firstName8,
+				age: $scope.age8,
+				hasResource: $scope.hasResource8 || false,
+				hasEmployer: $scope.hasEmployer8 || false
+			}
+		]
+	}
+// }
 
 
 	$scope.hh = 1;
 	$scope.addField = () => {
 		$scope.hh++;
-		console.log($scope.hh)
 	}
 
 //  MOVE TO APP FOR RESOLVE -- TODO
@@ -34,19 +98,27 @@ app.controller('DemoCtrl', function($scope, $route, personFactory, $location, $c
 				})
 	}
 
+	let newPerson = [];
+
 	$scope.getDemo = () => {
-		let newPerson = {
-			"householdId": $scope.householdId,
-			"firstName": $scope.firstName,
-			"age": $scope.age,
-			"hasResource": $scope.hasResource,
-			"hasEmployer": $scope.hasEmployer
+		$scope.getPeople()
+		let size = $scope.hh - 1;
+		let max = 7;
+		$scope.people.splice($scope.hh, max - size)
+		for (let i = 0; i < $scope.hh; i++) {
+				newPerson[i] = {
+					"householdId": $scope.householdId,
+					"firstName": $scope.people[i].firstName,
+					"age": $scope.people[i].age,
+					"hasResource": $scope.people[i].hasResource,
+					"hasEmployer": $scope.people[i].hasEmployer
 		}
-		console.log(newPerson)
-		// personFactory.createPerson(newPerson)
-		// .then((results) => {
-		// 	console.log(results)
-		// 	$scope.getPersonByHH(results._id)
-		// 	})
+		console.log(newPerson[i])
+		personFactory.createPerson(newPerson[i])
+		.then((results) => {
+			console.log(results)
+			// $scope.getPersonByHH(results._id)
+			})
+		}
 	}
 })
